9c3dd6e07250e695575fd739c1ef1c56ca6cef8f
RustamVally <rustamvaliullov@gmail.com>
2022-03-30 21:48:49 +0300

    getPage (Страницы со списком элементов)

--- src/main/java/org/example/controller/FlatController.java
+++ src/main/java/org/example/controller/FlatController.java
@@ -5,7 +5,10 @@ import org.example.manager.FlatManager;
 import org.example.model.Flat;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -17,6 +20,13 @@ public class FlatController {
     //  - read (getPage & getById)
     //  - update
     //  - delete
+
+    // TODO: http://localhost:9999/getPage?page=1
+    @RequestMapping("/getPage")
+    public List<Flat> getPage(@RequestParam int page) {
+        return manager.getPage(page);
+    }
+
 
     // TODO: http://localhost:9999/add?id=0&name=Flat&price=1000000&rooms=3&streets=Krasnaya&streets=Belaya
     @RequestMapping("/add")
--- src/main/java/org/example/manager/FlatManager.java
+++ src/main/java/org/example/manager/FlatManager.java
@@ -8,8 +8,15 @@ import java.util.List;
 
 @Component
 public class FlatManager {
+    private final int itemsPerPage = 5;
     private long nextId = 1;
     private List<Flat> items = new ArrayList<>();
+
+    public List<Flat> getPage(int page) {
+        final int fromIndex = (page - 1) * itemsPerPage;
+        final int toIndex = fromIndex + itemsPerPage;
+        return items.subList(fromIndex, toIndex);
+    }
 
     public Flat add(Flat item) {
         item.setId(nextId++);
